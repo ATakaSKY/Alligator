2efd4b820fe78da21313826c43649802732595d4
Aakash Thakur <thakurakash321@gmail.com>
2018-01-16 23:57:17 +0530

    Change detection

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChildComponent {
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  @Input() data: string[];
+
+  refresh() {
+    this.cd.detectChanges();
+  }
+
+}
