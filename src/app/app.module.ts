e0e89158f7caefff9361f423497abd2ec88e9f56
Aakash Thakur <thakurakash321@gmail.com>
2018-01-17 12:22:44 +0530

    Host binding and guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RainbowDirective } from './host-binding.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RainbowDirective
   ],
   imports: [
     BrowserModule
